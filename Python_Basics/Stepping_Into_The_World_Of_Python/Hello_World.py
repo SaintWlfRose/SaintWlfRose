05e234b7b3a77b55081c5a3c50200372a3c1bacc
Saint_Rose <97374571+SaintWlfRose@users.noreply.github.com>
2024-02-10 02:23:53 +0800

    Update Hello_World.py

--- Python_Basics/Stepping_Into_The_World_Of_Python/Hello_World.py
+++ Python_Basics/Stepping_Into_The_World_Of_Python/Hello_World.py
@@ -1,2 +1,16 @@
 #Print Hello World
+#The print function is used to display text in Python.
 print("Hello World")
+
+
+#Using the print function to add numbers.
+print(1 + 2) 
+
+#Using the pring function to multiply numbers
+print(7 * 6)
+
+#Using the print function to have an new line
+print()
+
+#Print out a string in Python
+print("The End")
